440c000e1210ea989388a0b05645bb8f4a1334a6
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-08-15 10:04:05 +0200

    15 aout 2023

--- features/employeeSlice.js
+++ features/employeeSlice.js
@@ -1,16 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState = [];                         // On créer le state initial (sa valeur de départ) pour la tranche (slice) de données. 
 
-const employeeSlice = createSlice({
+const employeeSlice = createSlice({              // Création d'unetranche (ou slice), c'est la combinaison d'un réducteur et de ses actions.
 	name: "employees",
 	initialState,
-	reducers: {
-		addEmployee: (state, action) => {
-			state.push(action.payload);
+	reducers: {                                  // Les réducteurs définissent comment l'état évolue en réponse aux actions.
+		addEmployee: (state, action) => {        // Le réducteur `addEmployee` ajoute un employé à l'état.
+			state.push(action.payload);          // Ajoute l'employé (fourni par `action.payload`) à l'état courant.
 		},
 	},
 });
 
-export const { addEmployee } = employeeSlice.actions;
-export default employeeSlice.reducer;
+export const { addEmployee } = employeeSlice.actions; // Exportation des actions générées par `createSlice`pour utiliser pour déclencher des changements d'état dasn les composants
+export default employeeSlice.reducer;                 // J'exporte ce réducteur pour créer le store Redux.
+
+
+
+
+
+           
+
+
+
--- store/store.js
+++ store/store.js
@@ -1,8 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
-import employeeReducer from "../features/employeeSlice";
+import { configureStore } from "@reduxjs/toolkit";         // On importe la fonction configureStore depuis la bibliothèque Redux Toolkit.
+import employeeReducer from "../features/employeeSlice";   // Cette fonction détermine comment l'état de l'application change en réponse à une action.
 
-export const store = configureStore({
-	reducer: {
-		employees: employeeReducer,
+export const store = configureStore({                      // Cette fonction permet de créer facilement un store Redux en fournissant simplement un objet de configuration.
+	reducer: {                                               // Ce réducteur spécifie comment l'état évolue en réponse aux actions envoyées au magasin.
+		employees: employeeReducer,                            // qui gère l'état sous la clé `employees`
 	},
 });
+
+// Un reducer est une fonction qui prend en compte l'état actuel de l'application et une action, 
+// puis renvoie un nouvel état basé sur cette action.
+
+// Crée et exporte le store Redux en utilisant la fonction configureStore. 
+// L'objet de configuration passé à cette fonction spécifie que l'état de l'application 
+// doit être divisé en deux sous-états 'user' et 'auth', chacun étant géré par son propre reducer.
